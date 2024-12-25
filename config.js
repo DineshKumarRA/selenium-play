6c3332f2b6bade2b5366598f142f2dbd8dfdee04
Dinesh Kumar R A <37109896+DineshKumarRA@users.noreply.github.com>
2024-12-25 22:19:13 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 module.exports = {
   platform: 'github',
   onboardingConfig: {
-    "extends": [ "config:recommended" ]
+    "extends": [
+      "config:base"
+    ]
   },
   enabledManagers: [ "bazel", "bazel-module", "bazelisk", "nuget", "maven", "npm", "pip_requirements", "pip_setup", "bundler", "ruby-version", "cargo" ],
-  allowedPostUpgradeCommands: [ 
-    "^REPIN=1 bazel run @maven//:pin$",
-    "^bazel run -- @pnpm//:pnpm install --dir $PWD --lockfile-only$",
-    "^CARGO_BAZEL_REPIN=true bazel sync --only=crates$",
-    "^bazel run //py:requirements.update$"
+  allowedPostUpgradeCommands: [
+    "REPIN=1 bazel run @maven//:pin",
+    "bazel run -- @pnpm//:pnpm install --dir $PWD --lockfile-only",
+    "CARGO_BAZEL_REPIN=true bazel sync --only=crates",
+    "bazel run //py:requirements.update"
   ]
 };
