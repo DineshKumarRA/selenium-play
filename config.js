2176bacf4522951fbc57b6f421059257549a84e5
Dinesh Kumar R A <37109896+DineshKumarRA@users.noreply.github.com>
2024-12-26 21:32:36 +0000

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,12 @@ module.exports = {
   enabledManagers: [ "bazel", "bazel-module", "bazelisk", "nuget", "maven", "npm", "pip_requirements", "pip_setup", "bundler", "ruby-version", "cargo" ],
   autodiscover: true,
   allowedPostUpgradeCommands: [
+    "sudo apt update",
+    "sudo apt install apt-transport-https curl gnupg -y",
+    "curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg",
+    "sudo mv bazel-archive-keyring.gpg /usr/share/keyrings",
+    "echo \"deb [signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8\" | sudo tee /etc/apt/sources.list.d/bazel.list",
+    "sudo apt update && sudo apt install bazel",
     "REPIN=1 bazel run @maven//:pin",
     "bazel run -- @pnpm//:pnpm install --dir $PWD --lockfile-only",
     "CARGO_BAZEL_REPIN=true bazel sync --only=crates",
