d219c0403fd3d0fa582b503fb2867274b85dbc70
Dinesh Kumar R A <37109896+DineshKumarRA@users.noreply.github.com>
2024-12-26 22:02:18 +0000

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
     'sudo apt install apt-transport-https curl gnupg -y',
     'curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg',
     'sudo mv bazel-archive-keyring.gpg /usr/share/keyrings',
-    'echo "deb [signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list',
+    'echo "deb [signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list > /dev/null',
     'sudo apt update && sudo apt install bazel',
     'REPIN=1 bazel run @maven//:pin',
     'bazel run -- @pnpm//:pnpm install --dir $PWD --lockfile-only',
