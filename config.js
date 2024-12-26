e3833e219f2129281f27103ec1bf8322de1e905b
Dinesh Kumar R A <37109896+DineshKumarRA@users.noreply.github.com>
2024-12-26 20:03:29 +0000

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ module.exports = {
   enabledManagers: [ "bazel", "bazel-module", "bazelisk", "nuget", "maven", "npm", "pip_requirements", "pip_setup", "bundler", "ruby-version", "cargo" ],
   autodiscover: true,
   allowedPostUpgradeCommands: [
-    "^REPIN=1 bazel run @maven//:pin$",
-    "^bazel run -- @pnpm//:pnpm install --dir $PWD --lockfile-only$",
-    "^CARGO_BAZEL_REPIN=true bazel sync --only=crates$",
-    "^bazel run //py:requirements.update$"
+    "REPIN=1 bazel run @maven//:pin",
+    "bazel run -- @pnpm//:pnpm install --dir $PWD --lockfile-only",
+    "CARGO_BAZEL_REPIN=true bazel sync --only=crates",
+    "bazel run //py:requirements.update"
   ]
 };
